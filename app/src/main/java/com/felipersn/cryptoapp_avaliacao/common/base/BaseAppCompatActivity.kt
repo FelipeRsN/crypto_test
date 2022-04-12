0fa9b9a93b886c9e872c7bb0e5f4765df24aded4
Felipe Silveira <felipe.rsneves@gmail.com>
2022-04-12 19:30:56 -0300

    created baseAppCompatActivity and refactored SignInActivity to use this baseClass | created new themes and changed dimens

---
+++ app/src/main/java/com/felipersn/cryptoapp_avaliacao/common/base/BaseAppCompatActivity.kt
@@ -0,0 +1,31 @@
+package com.felipersn.cryptoapp_avaliacao.common.base
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseAppCompatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initView()
+    }
+
+    protected open fun initView() {
+        setupIntentExtras()
+        setupScreen()
+        setupListeners()
+    }
+
+    protected open fun setupIntentExtras() {}
+    protected open fun setupScreen() {}
+    protected open fun setupListeners() {}
+
+    override fun onOptionsItemSelected(menuItem: MenuItem): Boolean {
+        if (menuItem.itemId == android.R.id.home) {
+            onBackPressed()
+        }
+        return super.onOptionsItemSelected(menuItem)
+    }
+}
+
