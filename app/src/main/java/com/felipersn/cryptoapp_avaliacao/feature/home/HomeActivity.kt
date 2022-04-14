a1087e37b07fa87208bffb840a8a542003c9fb9d
Felipe Silveira <felipe.rsneves@gmail.com>
2022-04-13 22:57:40 -0300

    working on home screen | list item layout

--- app/src/main/java/com/felipersn/cryptoapp_avaliacao/feature/home/HomeActivity.kt
+++ app/src/main/java/com/felipersn/cryptoapp_avaliacao/feature/home/HomeActivity.kt
@@ -3,10 +3,10 @@ package com.felipersn.cryptoapp_avaliacao.feature.home
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.felipersn.cryptoapp_avaliacao.R
+import com.felipersn.cryptoapp_avaliacao.common.base.BaseAppCompatActivity
+import com.felipersn.cryptoapp_avaliacao.databinding.ActivityHomeBinding
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : BaseAppCompatActivity() {
 
     companion object {
         fun getIntent(context: Context): Intent {
@@ -14,8 +14,15 @@ class HomeActivity : AppCompatActivity() {
         }
     }
 
+    private val homeBinding by lazy { ActivityHomeBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(homeBinding.root)
     }
+
+    override fun setupScreen() {
+        super.setupScreen()
+    }
+
 }
