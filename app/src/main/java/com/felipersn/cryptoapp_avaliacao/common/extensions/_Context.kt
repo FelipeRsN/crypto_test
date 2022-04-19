5940717566b811ebe9045272890b7db33c310c14
Felipe Silveira <felipe.rsneves@gmail.com>
2022-04-19 20:26:08 -0300

    working on HomeScreen, missing populate list | added enum class to handler the list visualization | added extension to head json from assets folder

--- app/src/main/java/com/felipersn/cryptoapp_avaliacao/common/extensions/_Context.kt
+++ app/src/main/java/com/felipersn/cryptoapp_avaliacao/common/extensions/_Context.kt
@@ -2,7 +2,18 @@ package com.felipersn.cryptoapp_avaliacao.common.extensions
 
 import android.content.Context
 import android.widget.Toast
+import java.io.IOException
 
 fun Context.toast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+@Throws(IOException::class)
+fun Context.readJsonAsset(fileName: String): String {
+    val inputStream = assets.open(fileName)
+    val size = inputStream.available()
+    val buffer = ByteArray(size)
+    inputStream.read(buffer)
+    inputStream.close()
+    return String(buffer, Charsets.UTF_8)
 }
--- app/src/main/java/com/felipersn/cryptoapp_avaliacao/common/extensions/_View.kt
+++ app/src/main/java/com/felipersn/cryptoapp_avaliacao/common/extensions/_View.kt
@@ -1,11 +1,21 @@
 package com.felipersn.cryptoapp_avaliacao.common.extensions
 
 import android.view.View
+import com.felipersn.cryptoapp_avaliacao.common.utils.DISABLED_VIEW_ALPHA
+import com.felipersn.cryptoapp_avaliacao.common.utils.ENABLED_VIEW_ALPHA
 
 fun View?.toggleEnabled(isEnabled: Boolean) {
     this?.let {
         it.isEnabled = isEnabled
         it.isClickable = isEnabled
-        it.alpha = if (isEnabled) 1f else 0.5f
+        if (isEnabled) it.enable() else it.disable()
     }
+}
+
+fun View?.enable() {
+    this?.let { it.alpha = ENABLED_VIEW_ALPHA }
+}
+
+fun View?.disable() {
+    this?.let { it.alpha = DISABLED_VIEW_ALPHA }
 }
